fac3e66652b19c6b0c62e90f39152e229a4443dc
liqianjie <3745983@qq.com>
2018-03-29 13:29:35 +0800

    te

--- main.ts
+++ main.ts
@@ -20,6 +20,34 @@ namespace Magibit {
         P16 = DigitalPin.P16
     }
 
+    export enum A1 {
+        P1 = DigitalPin.P1,
+        P2 = DigitalPin.P2
+    }
+
+    export enum A2 {
+        P0 = DigitalPin.P0,
+        P1 = DigitalPin.P1
+    }
+
+    export enum A12 {
+        A1 = 0x1,
+        A2 = 0x2
+    }
+
+    export enum ONOFF {
+        ON = 0x1,
+        OFF = 0x0
+    }
+
+    export enum Servos {
+        P0 = DigitalPin.P0,
+        P1 = DigitalPin.P1,
+        P2 = DigitalPin.P2,
+        P13 = DigitalPin.P13,
+        P16 = DigitalPin.P16
+    }
+
 
     /**
      * Read Light
@@ -60,6 +88,32 @@ namespace Magibit {
         return lightRead;
       }
 
+      //% blockId=maiji_Joystick_speed block="Joystick read speed at|%pin"
+      //% weight=81
+      export function JoystickSpeedRead(pin: A12): number{
+        let lightRead = 0;
+        if  (pin==0){
+          lightRead =  pins.digitalReadPin(DigitalPin.P1);
+          return lightRead;
+        }else{
+          lightRead =  pins.digitalReadPin(DigitalPin.P0);
+          return lightRead;
+        }
+      }
+
+      //% blockId=maiji_Joystick_angle block="Joystick read angle at|%pin"
+      //% weight=81
+      export function JoystickAngleRead(pin: A12): number{
+        let lightRead = 0;
+        if  (pin==0){
+          lightRead =  pins.digitalReadPin(DigitalPin.P2);
+          return lightRead;
+        }else{
+          lightRead =  pins.digitalReadPin(DigitalPin.P1);
+          return lightRead;
+        }
+      }
+
       //% blockId=maiji_infrared block="Infrared read digital at|%pin"
       //% weight=81
       export function InfraredRead(pin: P1316): number{
@@ -91,6 +145,109 @@ namespace Magibit {
         lightRead =  pins.digitalReadPin(pin);
         return lightRead;
       }
+
+
+
+      //% blockId=maiji_light_brightness block="Light Set brightness at|%pin brightness |%brightness"
+      //% weight=81
+      //% brightness.min=0 brightness.max=255
+      export function LightBrightnessWrite(pin: Light, brightness:number): void{
+        pins.digitalWritePin(pin, brightness);
+      }
+
+
+      //% blockId=maiji_light_Twinkle block="Light |%pin Twinkle |%twinkle|ms"
+      //% weight=81
+      export function LightTwinkleWrite(pin: Light, twinkle:number): void{
+        pins.digitalWritePin(pin, 255);
+        basic.pause(twinkle );
+        pins.digitalWritePin(pin, 0);
+      }
+
+      //% blockId=maiji_light_onoff block="Light |%pin status |%onoff"
+      //% weight=81
+      export function LightOnOffWrite(pin: Light, onoff:ONOFF): void{
+        if(onoff==1){
+          pins.digitalWritePin(pin, 255);
+        }else{
+          pins.digitalWritePin(pin, 0);
+        }
+      }
+
+      /**
+       * Servo Execute
+       * @param index Servo Channel; eg: P0
+       * @param degree [0-180] degree of servo; eg: 0, 90, 180
+      */
+        //% blockId=maji_servo block="Servo|%index|degree %degree"
+        //% weight=100
+        //% degree.min=0 degree.max=180
+        export function Servo(pin: Servos, degree: number): void {
+              pins.digitalWritePin(pin, degree);
+        }
+
+        /**
+         * Water Pump
+        */
+          //% blockId=waterpump_onoff block="Water Pump|%index|Status %onoff"
+          //% weight=100
+          //% degree.min=0 degree.max=180
+          export function WaterPumpOnoff(index: Motors, onoff: ONOFF): void {
+            if (index==1){
+              if (onoff==1){
+                pins.analogWritePin(AnalogPin.P12,1023);
+              }else{
+                pins.analogWritePin(AnalogPin.P12,0);
+              }
+
+            }else{
+              if (onoff==1){
+                pins.analogWritePin(AnalogPin.P15,1023);
+              }else{
+                pins.analogWritePin(AnalogPin.P15,0);
+              }
+            }
+          }
+
+
+          /**
+           * Water Pump
+          */
+            //% blockId=waterpump_onoff_delay block="Water Pump|%index|Status %onoff delay %delay ms"
+            //% weight=100
+            //% degree.min=0 degree.max=180
+            export function WaterPumpOnoffDelay(index: Motors, onoff: ONOFF, delay:number): void {
+              let speed=0;
+              if (index==1){
+
+                speed = pins.analogReadPin(AnalogPin.P12);
+
+                if (onoff==1){
+                  pins.analogWritePin(AnalogPin.P12,1023);
+                }else{
+                  pins.analogWritePin(AnalogPin.P12,0);
+                }
+
+                basic.pause(delay );
+
+                pins.analogWritePin(AnalogPin.P12,speed);
+
+              }else{
+
+                speed = pins.analogReadPin(AnalogPin.P15);
+
+                if (onoff==1){
+                  pins.analogWritePin(AnalogPin.P15,1023);
+                }else{
+                  pins.analogWritePin(AnalogPin.P15,0);
+                }
+
+                basic.pause(delay);
+
+                pins.analogWritePin(AnalogPin.P15,speed);
+              }
+            }
+
 
       //% blockId=maiji_Motor_Speed_Sensor block="Motor Speed Sensor read digital at|%pin"
       //% weight=81
@@ -132,13 +289,13 @@ namespace Magibit {
 	 * @param speed [-1023-1023] speed of motor; eg: 150, -150
 	 * @param delay seconde delay to stop; eg: 1
 	*/
-    //% blockId=maiji_motor_rundelay block="Motor|%index|speed %speed|delay %delay|s"
+    //% blockId=maiji_motor_rundelay block="Motor|%index|speed %speed|delay %delay|ms"
     //% weight=81
     //% speed.min=-1023 speed.max=1023
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function MotorRunDelay(index: Motors, speed: number, delay: number): void {
         MotorRun(index, speed);
-        basic.pause(delay * 1000);
+        basic.pause(delay );
         MotorStop(index);
     }
 
